5600f62b214ce6701580413e8d995307a4fefc74
Ollie <wilsonoliv3@ecs.vuw.ac.nz>
2017-02-18 18:46:22 +1300

    added if statement in flyout.show to check if passed a node or an array

--- core/flyout.js
+++ core/flyout.js
@@ -36,6 +36,7 @@ goog.require('goog.dom');
 goog.require('goog.events');
 goog.require('goog.math.Rect');
 goog.require('goog.userAgent');
+goog.require('goog.ui.tree.TreeNode');
 
 
 /**
@@ -722,7 +723,10 @@ Blockly.Flyout.prototype.hide = function() {
  *     Variables and procedures have a custom set of blocks.
  */
 Blockly.Flyout.prototype.show = function(node) {
-    var xmlList = node.blocks;
+var xmlList = node;
+    if(node instanceof goog.ui.tree.TreeNode) {
+    xmlList = node.blocks;
+    }
   this.workspace_.setResizesEnabled(false);
   this.hide();
   this.clearOldBlocks_();
